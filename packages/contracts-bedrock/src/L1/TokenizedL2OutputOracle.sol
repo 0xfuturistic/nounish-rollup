b2dbaa05ff4144a56baa76dd28e5a618daeca3c5
Diego <105765223+0xfuturistic@users.noreply.github.com>
2023-08-10 18:56:31 +0200

    add Proposer Account in TokenizedL2OutputOracle.sol

--- packages/contracts-bedrock/src/L1/TokenizedL2OutputOracle.sol
+++ packages/contracts-bedrock/src/L1/TokenizedL2OutputOracle.sol
@@ -1,32 +1,45 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
 
-import { L2OutputOracle } from "./L2OutputOracle.sol";
-import { Token as NounsERC721 } from "nouns-protocol/token/Token.sol";
+import {L2OutputOracle} from "./L2OutputOracle.sol";
+import {Token as NounsERC721} from "nouns-protocol/token/Token.sol";
+
+import {IERC6551Registry} from "erc6551/interfaces/IERC6551Registry.sol";
 
 contract TokenizedL2OutputOracle is NounsERC721, L2OutputOracle {
-    address constant DEFAULT_PROPOSER = address(0); /// @dev harcoded value in L2OutputOracle initialization
+    address public immutable DEFAULT_PROPOSER;
+    address public immutable ERC6551_REGISTRY;
+    address public immutable PROPOSER_ACCOUNT_IMPL;
 
     constructor(
         uint256 _submissionInterval,
         uint256 _l2BlockTime,
         uint256 _finalizationPeriodSeconds,
-        address _tokenManager
-    ) L2OutputOracle(_submissionInterval, _l2BlockTime, _finalizationPeriodSeconds) NounsERC721(_tokenManager) {}
+        address _tokenManager,
+        address _defaultProposer,
+        address _erc6551Registry,
+        address _proposerAccountImpl
+    ) L2OutputOracle(_submissionInterval, _l2BlockTime, _finalizationPeriodSeconds) NounsERC721(_tokenManager) {
+        DEFAULT_PROPOSER = _defaultProposer;
+        ERC6551_REGISTRY = _erc6551Registry;
+        PROPOSER_ACCOUNT_IMPL = _proposerAccountImpl;
+    }
 
     function proposeL2Output(bytes32 _outputRoot, uint256 _l2BlockNumber, bytes32 _l1BlockHash, uint256 _l1BlockNumber)
         public
         payable
         override
     {
-        uint tokenId = nextOutputIndex();
+        uint256 tokenId = nextOutputIndex();
 
-        if (owners[tokenId] == address(0)){
+        if (owners[tokenId] == address(0)) {
             /// @dev the token hasn't been minted, so DEFAULT_PROPOSER is the proposer
             proposer = DEFAULT_PROPOSER;
         } else {
             /// @dev the token has been minted, so the token's owner is the proposer
-            proposer = owners[tokenId];
+            proposer = IERC6551Registry(ERC6551_REGISTRY).account(
+                PROPOSER_ACCOUNT_IMPL, block.chainid, address(this), tokenId, 0
+            );
             _burn(tokenId);
         }
 
