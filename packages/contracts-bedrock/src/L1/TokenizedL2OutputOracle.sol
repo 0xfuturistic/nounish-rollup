e08e1088c9f1d3763f399664d86d08656947c228
Diego <105765223+0xfuturistic@users.noreply.github.com>
2023-08-10 18:22:45 +0200

    Rewrite TokenizedL2OutputOracle.sol
    reduce code so that new functions need not be declared

--- packages/contracts-bedrock/src/L1/TokenizedL2OutputOracle.sol
+++ packages/contracts-bedrock/src/L1/TokenizedL2OutputOracle.sol
@@ -1,69 +1,36 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
 
-import { ERC721Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
-import { AccessControlUpgradeable } from "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
-
 import { L2OutputOracle } from "./L2OutputOracle.sol";
+import { Token as NounsERC721 } from "nouns-protocol/token/Token.sol";
 
-contract TokenizedL2OutputOracle is L2OutputOracle, ERC721Upgradeable, AccessControlUpgradeable {
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-
+contract TokenizedL2OutputOracle is NounsERC721, L2OutputOracle {
     constructor(
         uint256 _submissionInterval,
         uint256 _l2BlockTime,
-        uint256 _finalizationPeriodSeconds
-    ) L2OutputOracle(_submissionInterval, _l2BlockTime, _finalizationPeriodSeconds) {}
+        uint256 _finalizationPeriodSeconds,
+        address _tokenManager
+    ) L2OutputOracle(_submissionInterval, _l2BlockTime, _finalizationPeriodSeconds) NounsERC721(_tokenManager) {}
 
-    function initialize(
-        uint256 _startingBlockNumber,
-        uint256 _startingTimestamp,
-        address _proposer,
-        address _challenger
-    ) public override reinitializer(2) {
-        super.initialize(_startingBlockNumber, _startingTimestamp, _proposer, _challenger);
-
-        __ERC721_init("L2OutputOracle", "PROPOSER");
-        __AccessControl_init();
-
-        _grantRole(MINTER_ROLE, msg.sender); // TODO: set as auctioneer's address
-    }
-
-    function supportsInterface(bytes4 interfaceId)
+    function proposeL2Output(bytes32 _outputRoot, uint256 _l2BlockNumber, bytes32 _l1BlockHash, uint256 _l1BlockNumber)
         public
-        view
-        override(ERC721Upgradeable, AccessControlUpgradeable)
-        returns (bool)
+        payable
+        override
     {
-        return super.supportsInterface(interfaceId);
-    }
+        uint tokenId = nextOutputIndex();
 
-    function proposeL2Output(
-        bytes32 _outputRoot,
-        uint256 _l2BlockNumber,
-        bytes32 _l1BlockHash,
-        uint256 _l1BlockNumber
-    ) public payable override {
-        _handleNextProposerNotMinted();
-        proposer = nextProposerAddress();
-
-        super.proposeL2Output(_outputRoot, _l2BlockNumber, _l1BlockHash, _l1BlockNumber);
-
-        _burn(nextOutputIndex());
-    }
-
-    function safeMint(address to, uint256 tokenId) public onlyRole(MINTER_ROLE) {
-        _safeMint(to, tokenId);
-    }
-
-    function nextProposerAddress() public view returns (address) {
-        return ownerOf(nextOutputIndex()); // todo: get erc6551 account address
-    }
-
-    function _handleNextProposerNotMinted() internal {
-        uint256 l2OutputIndex = nextOutputIndex();
-        if (!_exists(l2OutputIndex)) {
-            _safeMint(proposer, l2OutputIndex);
+        if (owners[tokenId] == address(0)){
+            /// @dev the token hasn't been minted, so only allow default proposer to propose
+            super.proposeL2Output(_outputRoot, _l2BlockNumber, _l1BlockHash, _l1BlockNumber);
+        } else {
+            /// @dev the token has been minted, so only allow the owner to propose. override
+            ///      the proposer so that it's picked up by L2OutputOracle and then reset it
+            ///      to the default proposer so we don't lose the latter.
+            address defaultProposer = proposer;
+            proposer = owners[tokenId];
+            super.proposeL2Output(_outputRoot, _l2BlockNumber, _l1BlockHash, _l1BlockNumber);
+            proposer = defaultProposer;
+            _burn(tokenId);
         }
     }
 }
